cc57dd35796a56ccf14b5acaa97d0b693ba1ee7d
chitreshmalhan2000 <161840338+chitreshmalhan2000@users.noreply.github.com>
2024-03-24 13:11:14 +0530

    Practice_Project2.java

---
+++ Practice_Project2/Practice_Project2.java
@@ -0,0 +1,42 @@
+package Practice_Project2;
+
+class DefaultAccessModifier {
+    void display() {
+        System.out.println("This is a default access modifier.");
+    }
+}
+public class Practice_Project2 {
+    public static void main(String[] args) {
+        DefaultAccessModifier defaultAccessModifier = new DefaultAccessModifier();
+        defaultAccessModifier.display();
+
+        PublicAccessModifier publicAccessModifier = new PublicAccessModifier();
+        publicAccessModifier.display();
+
+        PrivateAccessModifier privateAccessModifier = new PrivateAccessModifier();
+         // privateAccessModifier.display();
+        // Won't compile because the method is private and cannot be accessed from outside the class
+        
+        ProtectedAccessModifier protectedAccessModifier = new ProtectedAccessModifier();
+        protectedAccessModifier.display();
+    }
+}
+
+class PublicAccessModifier {
+    public void display() {
+        System.out.println("This is a public access modifier.");
+    }
+}
+
+
+class PrivateAccessModifier {
+    private void display() {
+        System.out.println("This is a private access modifier.");
+    }
+}
+
+class ProtectedAccessModifier {
+    protected void display() {
+        System.out.println("This is a protected access modifier.");
+    }
+}
