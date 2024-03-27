65eb057f29f825449fa7091db754022847348061
chitreshmalhan2000 <161840338+chitreshmalhan2000@users.noreply.github.com>
2024-03-27 23:25:58 +0530

    Practice_Project18

--- Practice_Project18/Practice_Project18.java
+++ Practice_Project18/Practice_Project18.java
@@ -14,3 +14,71 @@ public class Practice_Project18 {
         electricCarVehicle.accelerate();
     }
 }
+//Encapsulation: Encapsulate the attributes of a car within a Car class
+public class Car {
+private String make;
+private String model;
+private int year;
+public Car(String make, String model, int year) {
+   this.make = make;
+   this.model = model;
+   this.year = year;
+}
+
+public String getMake() {
+   return make;
+}
+
+public String getModel() {
+   return model;
+}
+
+public int getYear() {
+   return year;
+}
+}
+
+//Concrete implementation of Vehicle for Car
+public class CarVehicle extends Vehicle {
+@Override
+public void accelerate() {
+   System.out.println("Car is accelerating.");
+}
+}
+
+//Inheritance: Create a subclass ElectricCar that inherits from Car
+public class ElectricCar extends Car {
+public int getBatteryCapacity() {
+		return batteryCapacity;
+	}
+
+	public void setBatteryCapacity(int batteryCapacity) {
+		this.batteryCapacity = batteryCapacity;
+	}
+
+private int batteryCapacity;
+public ElectricCar(String make, String model, int year, int batteryCapacity) {
+   super(make, model, year);
+   this.batteryCapacity = batteryCapacity;
+}
+
+// Polymorphism: Override the getYear method to add extra functionality
+@Override
+public int getYear() {
+   int currentYear = super.getYear();
+   return currentYear - 5; // Simulating depreciation
+}
+}
+//Concrete implementation of Vehicle for ElectricCar
+public class ElectricCarVehicle extends Vehicle {
+@Override
+public void accelerate() {
+   System.out.println("Electric car is accelerating silently.");
+}
+}
+//Abstraction: Create an abstract class Vehicle with abstract method accelerate
+public abstract class Vehicle {
+public abstract void accelerate();
+}
+
+
