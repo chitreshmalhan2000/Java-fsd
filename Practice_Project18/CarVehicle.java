d7563118a00302ffed968dca83aff8c01b89960c
chitreshmalhan2000 <161840338+chitreshmalhan2000@users.noreply.github.com>
2024-03-27 23:27:38 +0530

    Practice_Project18

--- Practice_Project18/CarVehicle.java
+++
@@ -1,8 +0,0 @@
-package Practice_Project18;
-//Concrete implementation of Vehicle for Car
-public class CarVehicle extends Vehicle {
- @Override
- public void accelerate() {
-     System.out.println("Car is accelerating.");
- }
-}
