93df315d9e4cc5e9b36e7d0d42e3a47dea9cfc07
chitreshmalhan2000 <161840338+chitreshmalhan2000@users.noreply.github.com>
2024-03-27 23:03:35 +0530

    Practice_Project11.java

--- Practice_Project11/Practice_Project11.java
+++ Practice_Project11/Practice_Project11.java
@@ -1,10 +1,22 @@
 package Practice_Project11;
 public class Practice_Project11 {
- public static void main(String[] args) {
-     MyThread thread1 = new MyThread();
-     thread1.start();
-     MyRunnable myRunnable = new MyRunnable();
-     Thread thread2 = new Thread(myRunnable);
-     thread2.start();
- }
-}
+	 public static void main(String[] args) {
+	     MyThread thread1 = new MyThread();
+	     thread1.start();
+	     MyRunnable myRunnable = new MyRunnable();
+	     Thread thread2 = new Thread(myRunnable);
+	     thread2.start();
+	 }
+	}
+class MyRunnable implements Runnable {
+	 public void run() {
+	     System.out.println("Thread created by implementing Runnable interface.");
+	 }
+	}
+class MyThread extends Thread {
+	 public void run() {
+	     System.out.println("Thread created by extending Thread class.");
+	 }
+	}
+
+
