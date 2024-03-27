b93a0524ed2ab32703df5463fa3b417244fe9bd7
chitreshmalhan2000 <161840338+chitreshmalhan2000@users.noreply.github.com>
2024-03-27 21:40:09 +0530

    Counter.java

---
+++ Practice_Project13/Counter.java
@@ -0,0 +1,15 @@
+package Practice_Project13;
+public class Counter {
+    private int count = 0;
+    public synchronized void increment() {
+        count++;
+    }
+
+    public synchronized void decrement() {
+        count--;
+    }
+
+    public synchronized int getCount() {
+        return count;
+    }
+}
