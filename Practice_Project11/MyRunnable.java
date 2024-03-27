b3d835fa0de684318da7a42a2f3881a0fdf5f33e
chitreshmalhan2000 <161840338+chitreshmalhan2000@users.noreply.github.com>
2024-03-27 22:40:18 +0530

    Practice_Project11

--- Practice_Project11/MyRunnable.java
+++ Practice_Project11/MyRunnable.java
@@ -1,5 +1,5 @@
 package Practice_Project11;
-class MyRunnable implements Runnable {
+public class MyRunnable implements Runnable {
 	 public void run() {
 	     System.out.println("Thread created by implementing Runnable interface.");
 	 }
