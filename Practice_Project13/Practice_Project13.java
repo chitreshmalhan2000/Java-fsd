d339b8789b078aea7e9d85253dfb8d7e479880c4
chitreshmalhan2000 <161840338+chitreshmalhan2000@users.noreply.github.com>
2024-03-27 23:11:51 +0530

    Practice_Project13

--- Practice_Project13/Practice_Project13.java
+++ Practice_Project13/Practice_Project13.java
@@ -19,3 +19,39 @@ public class Practice_Project13 {
         System.out.println("Final count: " + counter.getCount());
     }
 }
+public class Counter {
+    private int count = 0;
+    public synchronized void increment() {
+        count++;
+    }
+
+    public synchronized void decrement() {
+        count--;
+    }
+
+    public synchronized int getCount() {
+        return count;
+    }
+}
+public class WorkerThread extends Thread {
+    private Counter counter;
+    private boolean increment;
+
+    public WorkerThread(Counter counter, boolean increment) {
+        this.counter = counter;
+        this.increment = increment;
+    }
+
+    public void run() {
+        if (increment) {
+            for (int i = 0; i < 1000; i++) {
+                counter.increment();
+            }
+        } else {
+            for (int i = 0; i < 1000; i++) {
+                counter.decrement();
+            }
+        }
+    }
+}
+
