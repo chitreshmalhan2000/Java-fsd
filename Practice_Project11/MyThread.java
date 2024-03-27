cf3f57b577c065fc507800e8d0cc0c631ecb97e9
chitreshmalhan2000 <161840338+chitreshmalhan2000@users.noreply.github.com>
2024-03-27 22:41:22 +0530

    Practice_Project11

--- Practice_Project11/MyThread.java
+++ Practice_Project11/MyThread.java
@@ -1,5 +1,5 @@
 package Practice_Project11;
-class MyThread extends Thread {
+public class MyThread extends Thread {
 	 public void run() {
 	     System.out.println("Thread created by extending Thread class.");
 	 }
