8432e3af5a5d62b3c5e35e2c32fca7d5eb62b228
chitreshmalhan2000 <161840338+chitreshmalhan2000@users.noreply.github.com>
2024-03-27 23:34:54 +0530

    Practice_Project19

--- Practice_Project19/Practice_Project19.java
+++ Practice_Project19/Practice_Project19.java
@@ -7,3 +7,32 @@ public class Practice_Project19 {
 	        animal.meow(); // Output: Cat is meowing.
 	    }
 	}
+//Common interface
+public interface Animal {
+void eat();
+}
+//Interface inheriting from Animal
+public interface Cat extends Animal {
+void meow();
+}
+//Interface inheriting from Animal
+public interface Dog extends Animal {
+void bark();
+}
+//Class implementing both Dog and Cat interfaces
+class AnimalImpl implements Dog, Cat {
+@Override
+public void eat() {
+   System.out.println("Animal is eating.");
+}
+
+@Override
+public void bark() {
+   System.out.println("Dog is barking.");
+}
+
+@Override
+public void meow() {
+   System.out.println("Cat is meowing.");
+}
+}
