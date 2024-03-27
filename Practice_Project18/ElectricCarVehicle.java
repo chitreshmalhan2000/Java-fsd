71c2c06d057cb8a2bd1ee44542f3a6ef697f5fa3
chitreshmalhan2000 <161840338+chitreshmalhan2000@users.noreply.github.com>
2024-03-27 23:28:51 +0530

    Practice_Project18

--- Practice_Project18/ElectricCarVehicle.java
+++
@@ -1,8 +0,0 @@
-package Practice_Project18;
-//Concrete implementation of Vehicle for ElectricCar
-public class ElectricCarVehicle extends Vehicle {
- @Override
- public void accelerate() {
-     System.out.println("Electric car is accelerating silently.");
- }
-}
